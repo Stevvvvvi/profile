7f9a0905c3fd76aeaaded55ee41e118d8fe50060
Stevvvvvi <lly363893886@gmail.com>
2020-11-02 19:45:33 +1100

    add icon

--- src/component/header/header.component.tsx
+++ src/component/header/header.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ArchiveFill, BarChartLineFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 
 import './header.styles.scss';
@@ -8,9 +9,9 @@ const Header=()=>{
         <div className='header'>
             <Link className="logo" to='/'><h2>STEVE LI</h2></Link>
             <div className='options'>
-                <Link className='option' to='project'>Projects</Link>
+                <Link className='option' to='project'><BarChartLineFill className='header-icon'/>Projects</Link>
                 <Link className='option' to='about'>About</Link>
-                <a className='option' href="/resume2.pdf" download>Resume</a>
+                <a className='option' href="/resume2.pdf" download><ArchiveFill className='header-icon'/>Resume</a>
 
             </div>
         </div>
